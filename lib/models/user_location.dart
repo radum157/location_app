14e0059773eb928d84f4456e9845a2dfd9c6a0cb
empty107 <83579540+empty107@users.noreply.github.com>
2021-05-07 18:16:37 +0300

    Delete lib directory

--- lib/models/user_location.dart
+++
@@ -1,8 +0,0 @@
-
-// Stores the latitude and longitude of the user's device
-class UserLocation {
-  final double latitude;
-  final double longitude;
-
-  UserLocation(this.latitude, this.longitude);
-}
