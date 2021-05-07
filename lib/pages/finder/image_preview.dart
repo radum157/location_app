86eea21a27f3acec3c4b4d5d9c7f9956a261b99c
empty107 <83579540+empty107@users.noreply.github.com>
2021-05-07 19:35:34 +0300

    Add files via upload

---
+++ lib/pages/finder/image_preview.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+// Preview uploaded image page
+
+class RenderImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final File image = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Text(basename(image.path)),
+      ),
+      body: Image.file(
+        image,
+        height: MediaQuery.of(context).size.height,
+      ),
+    );
+  }
+}
