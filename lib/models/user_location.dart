94af9871f03598b92d8d9c1fee5d0752dad1ba67
empty107 <83579540+empty107@users.noreply.github.com>
2021-05-07 18:17:01 +0300

    Add files via upload

---
+++ lib/models/user_location.dart
@@ -0,0 +1,8 @@
+
+// Stores the latitude and longitude of the user's device
+class UserLocation {
+  final double latitude;
+  final double longitude;
+
+  UserLocation(this.latitude, this.longitude);
+}
