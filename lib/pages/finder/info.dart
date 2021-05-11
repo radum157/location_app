e77f93803a0753a6c8765e79b192e6e0213e7569
empty107 <83579540+empty107@users.noreply.github.com>
2021-05-11 23:29:45 +0300

    Add files via upload

---
+++ lib/pages/finder/info.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatelessWidget {
+  const InfoPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: Text('Important'), backgroundColor: Colors.green[700]),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 20.0, 0.0, 2.0),
+              child: Text(
+                'Please make sure you have entered the correct information for each field.',
+                style: TextStyle(
+                  color: Colors.red[700],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 2.0),
+              child: Text(
+                'Images may take some time to upload.',
+                style: TextStyle(
+                  color: Colors.red[700],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/tracker/devices_list.dart
@@ -0,0 +1,159 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/models/snack_bar.dart';
+import 'package:flutter_app/models/user_location.dart';
+import 'package:flutter_app/pages/finder/finder.dart';
+import 'package:flutter_app/pages/tracker/add.dart';
+import 'package:flutter_app/pages/tracker/map.dart';
+import 'package:provider/provider.dart';
+
+class DeviceListPage extends StatefulWidget {
+  // Stores the id as created in the database
+  // of the current device if it's connected
+  static String id = '';
+
+  @override
+  _DeviceListPageState createState() => _DeviceListPageState();
+}
+
+class _DeviceListPageState extends State<DeviceListPage> {
+  final _firebaseUser = FirebaseAuth.instance.currentUser;
+  final _devices = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .collection('active');
+
+  Future<void> _updateLocation(UserLocation location) async {
+    if (location == null || DeviceListPage.id.isEmpty) return;
+    try {
+      await _devices.doc(DeviceListPage.id).get().then((document) {
+        document.data()['lat'] = location.latitude;
+        document.data()['lng'] = location.longitude;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final location = Provider.of<UserLocation>(context);
+
+    // this makes sure the location is updated when the device moves
+    if (DeviceListPage.id.isNotEmpty) {
+      _updateLocation(location);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Text('Your current devices'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              if (DeviceListPage.id.isEmpty) {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacementNamed(context, '/authWrapper');
+              } else {
+                buildSnackBar(context,
+                    'Please disconnect the current device before logging out.');
+              }
+            },
+            icon: Icon(
+              Icons.logout,
+              color: Colors.redAccent[700],
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.post_add),
+            onPressed: () {
+              if (_firebaseUser != null && _firebaseUser.emailVerified == true) {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Finder()));
+              } else {
+                buildSnackBar(context,
+                    'Please verify your email and log in again to access this feature.');
+              }
+            },
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: _devices.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData == false || snapshot.data.docs.length == 0) {
+            return Center(
+              child: Text('No devices detected.'),
+            );
+          }
+          return ListView(
+            children: snapshot.data.docs.map((document) {
+              return ListTile(
+                title: Text(
+                  document.data()['name'],
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  document.data()['number'],
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () async {
+                    if (document.id == DeviceListPage.id) {
+                        DeviceListPage.id = '';
+                    }
+                    await _devices.doc(document.id).delete();
+                  },
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MapPage(),
+                      settings: RouteSettings(
+                        arguments: {
+                          'name': document.data()['name'],
+                          'lat': document.data()['lat'],
+                          'lng': document.data()['lng'],
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green[700],
+        child: Icon(Icons.add_outlined),
+        // Go to add devices page
+        onPressed: () {
+          if (_firebaseUser == null) {
+            buildSnackBar(context, 'Please log in to access this feature.');
+          }
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddDevicePage(),
+              settings: RouteSettings(
+                name: 'devices',
+                arguments: _devices,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
