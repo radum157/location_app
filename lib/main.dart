09b3eb6dc291b85661d1065cd09e831053f6822e
empty107 <83579540+empty107@users.noreply.github.com>
2021-05-12 15:39:57 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/pages/auth_login/login.dart';
+import 'package:flutter_app/pages/home/welcome.dart';
+import 'package:flutter_app/services/authentication.dart';
+import 'package:flutter_app/services/location.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  // Initialize the app with Firebase and Flutter
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    // Set up providers for the authentication and location services
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (context) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+          initialData: null,
+        ),
+        StreamProvider(
+          create: (context) => LocationService().locationStream,
+          initialData: null,
+        ),
+      ],
+      child: MaterialApp(
+        initialRoute: '/authWrapper',
+        routes: {
+          '/authWrapper': (context) => AuthWrapper(),
+        },
+      ),
+    );
+  }
+}
+
+// Checks if the user is authenticated
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return WelcomePage();
+    }
+    return LoginPage();
+  }
+}
---
+++ lib/pages/tracker/map.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/models/snack_bar.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// Google maps location page
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> _device =
+        ModalRoute.of(context).settings.arguments;
+    final LatLng _position = LatLng(_device['lat'], _device['lng']);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Text('${_device['name']}\'s last known location'),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition:
+                CameraPosition(target: _position, zoom: 20.0),
+            markers: {
+              Marker(
+                position: _position,
+                markerId: MarkerId(_position.toString()),
+                infoWindow: InfoWindow(
+                  title: _device['name'],
+                  snippet:
+                      'lat: ${_position.latitude}, lng: ${_position.longitude}',
+                  onTap: () {
+                    Clipboard.setData(ClipboardData(
+                        text: '${_position.latitude} ${_position.longitude}'));
+                    buildSnackBar(
+                        context, 'Latitude and longitude copied to clipboard.');
+                  },
+                ),
+              ),
+            },
+            mapType: MapType.hybrid,
+          ),
+        ],
+      ),
+    );
+  }
+}
